627f0e9f14da729e36ea21a2aa92333cb73dbc22
nelsonfornazeiro <nelsonfornazeiro@gmail.com>
2026-01-22 11:47:53 -0300

    feat: creating domain user and tokenService

---
+++ src/main/java/com/full_events/eventos_api/domain/User.java
@@ -0,0 +1,25 @@
+package com.full_events.eventos_api.domain;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "user_tb")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+    private String email;
+    private String password;
+
+}
---
+++ src/test/java/com/full_events/eventos_api/EventosApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.full_events.eventos_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventosApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
