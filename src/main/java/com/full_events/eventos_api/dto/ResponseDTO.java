928fcac93edf9e6e6698eb14136bb75d85840620
nelsonfornazeiro <nelsonfornazeiro@gmail.com>
2026-01-22 12:47:28 -0300

    feat: creating jwt config

---
+++ src/main/java/com/full_events/eventos_api/dto/ResponseDTO.java
@@ -0,0 +1,4 @@
+package com.full_events.eventos_api.dto;
+
+public record ResponseDTO(String name, String token) {
+}
--- src/main/java/com/full_events/eventos_api/infra/security/TokenService.java
+++ src/main/java/com/full_events/eventos_api/infra/security/TokenService.java
@@ -4,7 +4,7 @@ import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.exceptions.JWTCreationException;
 import com.auth0.jwt.exceptions.JWTVerificationException;
-import com.full_events.eventos_api.domain.User;
+import com.full_events.eventos_api.domain.user.User;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
@@ -14,47 +14,37 @@ import java.time.ZoneOffset;
 
 @Service
 public class TokenService {
-
     @Value("${api.security.token.secret}")
     private String secret;
-
-    public String generateToken(User user) {
-        try{
+    public String generateToken(User user){
+        try {
             Algorithm algorithm = Algorithm.HMAC256(secret);
 
-           String token = JWT.create()
-                   .withIssuer("api-event")
-                   .withSubject(user.getEmail())
-                   .withExpiresAt(this.generateExpirationDate())
-                   .sign(algorithm);
+            String token = JWT.create()
+                    .withIssuer("login-auth-api")
+                    .withSubject(user.getEmail())
+                    .withExpiresAt(this.generateExpirationDate())
+                    .sign(algorithm);
             return token;
-
-        }catch (JWTCreationException exception){
+        } catch (JWTCreationException exception){
             throw new RuntimeException("Error while authenticating");
         }
     }
 
-    public String validadeToken(String token){
-        try{
+    public String validateToken(String token){
+        try {
             Algorithm algorithm = Algorithm.HMAC256(secret);
-
             return JWT.require(algorithm)
-                    .withIssuer("api-event")
+                    .withIssuer("login-auth-api")
                     .build()
                     .verify(token)
                     .getSubject();
-
-        }catch (JWTVerificationException exception){
-            throw new RuntimeException("Error while authenticating");
+        } catch (JWTVerificationException exception) {
+            return null;
         }
     }
 
-
-    public Instant generateExpirationDate(){
+    private Instant generateExpirationDate(){
         return LocalDateTime.now().plusHours(2).toInstant(ZoneOffset.of("-03:00"));
     }
-
-
 }
-
-
